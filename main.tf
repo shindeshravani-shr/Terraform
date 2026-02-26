91aa2cee88d345483cb1f7412eb9274bdd51a7d5
shindeshravani-shr <shindeshravani1984@gmail.com>
2026-02-26 19:46:18 +0530

    aws instance

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"
   
 }
-resource "aws_instance" "example" {
+resource "aws_instance" "examplee" {
   ami           = "ami-0f3caa1cf4417e51b"
   instance_type = "t3.micro"
 
